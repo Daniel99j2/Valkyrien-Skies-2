6971afdf24b25248692fe2c184d6e6884cede2f8
Daniel99j <121533735+Daniel99j2@users.noreply.github.com>
2026-04-14 10:52:33 +0800

    Update MixinLevelRendererVanilla.java

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/vanilla_renderer/MixinLevelRendererVanilla.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/vanilla_renderer/MixinLevelRendererVanilla.java
@@ -233,7 +233,8 @@ public abstract class MixinLevelRendererVanilla implements LevelRendererDuck, Le
 
     @WrapOperation(
         method = "*",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;distSqr(Lnet/minecraft/core/Vec3i;)D")
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;distSqr(Lnet/minecraft/core/Vec3i;)D"),
+        require = 0
     )
     private double distToShips(BlockPos from, Vec3i to, Operation<Double> distSqr){
         return VSGameUtilsKt.squaredDistanceBetweenInclShips(level, from.getCenter(), Vec3.atCenterOf(to), distSqr);
